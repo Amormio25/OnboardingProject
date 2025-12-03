9f556249f2a8b75eba273f5de1aa51d212549c93
Amormio Velasquez III <amorbuks25@gmail.com>
2025-12-02 18:28:18 -0800

    Importing task 1 from a different repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,39 @@
 import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{" "}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+  return (
+    <div className="h-screen w-screen overflow-auto bg-gray-50 sm:overflow-hidden md:overflow-hidden">
+      <div className="m-8 flex h-[calc(100%-4rem)] w-[calc(100%-4rem)] flex-col items-center justify-center overflow-auto rounded-xl border bg-white p-8 pt-16 shadow-lg sm:overflow-hidden md:overflow-hidden">
+        <h1 className="mb-4 pt-8 text-3xl font-semibold md:pt-0 md:text-5xl">
+          About Me
+        </h1>
+        <h3 className="text-lg md:text-xl">Hi, I'm Amormio!</h3>
+        <div className="grid h-full w-full grid-cols-1 items-center justify-items-center text-center md:grid-cols-[1fr_2fr_1fr]">
+          <div id="intro" className="min-w-52 p-8 text-left">
+            I'm a sophomore majoring in computer science and I've been trying to
+            learn more web development over the past year. I'm passionate about
+            learning and hope to become a software engineer in the future.
+          </div>
+          <Image
+            src="/assets/images/selfpic.jpg"
+            alt="self-pic"
+            className="h-auto w-48 max-w-full rounded-full object-cover md:w-2/3 md:min-w-48"
+            width={200}
+            height={200}
+            priority
+          />
+          <div id="interests" className="pb-8 pt-4 md:p-4">
+            <h3 className="mb-4 text-xl font-semibold">My Interests</h3>
+            <ul className="items-left flex min-w-52 list-disc flex-col text-center">
+              <li className="list-inside p-0">Playing guitar</li>
+              <li className="list-inside p-0">Watching movies/shows/anime</li>
+              <li className="list-inside p-0">Training Muay Thai</li>
+              <li className="list-inside p-0">Playing/watching basketball</li>
+              <li className="list-inside p-0">Web Dev</li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
